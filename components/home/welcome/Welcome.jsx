db41f7f37d296ef0809ff730dd059c28c87b4045
colinprize <109234316+colinprize@users.noreply.github.com>
2023-10-15 14:26:13 -0700

    font added

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -14,7 +14,11 @@ const Welcome = () => {
         <Text style={styles.welcomeMessage}> Find your perfect job</Text>
         </View>
         <View style={styles.searchContainer}>
-
+            <View style={styles.searchWrapper}>
+              <TextInput
+                style={styles.searchInput}
+              />
+            </View>
           </View>
     </View>
   )
